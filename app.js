c632916701b4d2548a1fdcb83786b13c0395b3c7
Sewn <sewnguyenp2206@gmail.com>
2026-07-16 10:41:43 +0700

    Test lộ secret

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
 
 app.get('/', (req, res) => {
     res.json({
