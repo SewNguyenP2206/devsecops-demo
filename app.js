c15de9f029472ad0287bedca2971c8eefe2826db
Sewn <sewnguyenp2206@gmail.com>
2026-07-16 10:45:13 +0700

    Pass tests

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const GITHUB_TOKEN = "ghp_1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r";
 
 app.get('/', (req, res) => {
     res.json({
